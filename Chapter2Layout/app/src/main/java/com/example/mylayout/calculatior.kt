27e92c2f817cac20ccaed569cf82a6727ea51990
Minji Kang <1117minsky@hufs.ac.kr>
2023-05-02 02:32:53 +0900

    ê°[êch2

---
+++ Chapter2Layout/app/src/main/java/com/example/mylayout/calculatior.kt
@@ -0,0 +1,11 @@
+package com.example.mylayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class calculatior : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculatior)
+    }
+}
---
+++ Chapter2Layout/app/src/main/java/com/example/mylayout/naver_login.kt
@@ -0,0 +1,11 @@
+package com.example.mylayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class naver_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_naver_login)
+    }
+}
