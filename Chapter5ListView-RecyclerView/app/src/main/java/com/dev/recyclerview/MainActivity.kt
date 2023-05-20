9b44341f924ae4dc5b07eff116668104fd0dacd2
Minji Kang <1117minsky@hufs.ac.kr>
2023-05-20 22:03:08 +0900

    feat: recyclerview, btn 작성

---
+++ Chapter5ListView-RecyclerView/app/src/main/java/com/dev/recyclerview/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.dev.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AbsListView.RecyclerListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dev.recyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    //미리 뷰바인딩 객체와 어댑터 객체 선언
+
+
+    //데이터 넣을 리스트 선언
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //view binding 처리
+
+    }
+
+}
